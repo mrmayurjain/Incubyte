b175558a996fe817232c2e9b4f3464b1376e6ff2
mrmayurjain <mayurjain.2809@gmail.com>
2020-10-08 23:03:12 +0530

    Logic for test cases 'allDividedByHundredAndNotByFourHundred5' is developed

--- LeapYearAssessment/src/com/mayur/incubyte/assessment/bll/LeapYearAssessment.java
+++ LeapYearAssessment/src/com/mayur/incubyte/assessment/bll/LeapYearAssessment.java
@@ -3,9 +3,10 @@ package com.mayur.incubyte.assessment.bll;
 public class LeapYearAssessment {
 	
 	private static final int FOUR_HUNDRED = 400; 
+	private static final int HUNDRED = 100; 
 
 	public boolean isLeapYear(int year) {
-		if(year % FOUR_HUNDRED == 0) {
+		if(year % FOUR_HUNDRED == 0 && year % HUNDRED ==0) {
 			return true;
 		}
 		return false;
