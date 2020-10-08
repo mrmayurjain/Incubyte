e7092f775d618caec2ff00e1bee4d56c5fa17e22
mrmayurjain <mayurjain.2809@gmail.com>
2020-10-08 23:24:22 +0530

    Code Refactor

--- LeapYearAssessment/src/com/mayur/incubyte/assessment/bll/LeapYearAssessment.java
+++ LeapYearAssessment/src/com/mayur/incubyte/assessment/bll/LeapYearAssessment.java
@@ -1,23 +1,39 @@
 package com.mayur.incubyte.assessment.bll;
 
 public class LeapYearAssessment {
-	
-	private static final int FOUR_HUNDRED = 400; 
-	private static final int HUNDRED = 100; 
-	private static final int FOUR = 4; 
+
+	private static final int FOUR_HUNDRED = 400;
+	private static final int HUNDRED = 100;
+	private static final int FOUR = 4;
 
 	public boolean isLeapYear(int year) {
-		if(year % FOUR != 0) {
+		if (!isDivisibleByFour(year)) {
 			return false;
 		}
-		if(year % FOUR == 0 && year % HUNDRED !=0) {
+		if (isDivisibleByFour(year) && !isDivisibleByHundred(year)) {
 			return true;
 		}
-		if(year % FOUR_HUNDRED == 0 && year % HUNDRED ==0) {
+		if (isDivisibleByFourHundred(year) && isDivisibleByHundred(year)) {
 			return true;
 		}
-		
 		return false;
 	}
 
+	private static boolean isDivisibleByFour(int divident) {
+		return isDivisible(divident, FOUR);
+	}
+
+	private static boolean isDivisibleByHundred(int divident) {
+		return isDivisible(divident, HUNDRED);
+	}
+
+	private static boolean isDivisibleByFourHundred(int divident) {
+		return isDivisible(divident, FOUR_HUNDRED);
+	}
+
+	private static boolean isDivisible(int divident, int divisor) {
+		if (divident % divisor == 0)
+			return true;
+		return false;
+	}
 }
--- LeapYearAssessment/src/com/mayur/incubyte/assessment/test/LeapYearTest.java
+++ LeapYearAssessment/src/com/mayur/incubyte/assessment/test/LeapYearTest.java
@@ -10,165 +10,131 @@ class LeapYearTest {
 
 	@Test
 	void allDividedByFourHundred() {
-		
 		int year = 2000;
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(!isLeapYear) {
+		if (!isLeapYear) {
 			fail("Test Case 'allDividedByFourHundred' has failed");
 		}
-		
 	}
-	
+
 	@Test
-	void allDividedByHundredAndNotByFourHundred1() {		
-		
+	void allDividedByHundredAndNotByFourHundred1() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 1700;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(isLeapYear) {
+		if (isLeapYear) {
 			fail("Test Case 'allDividedByHundredAndNotByFourHundred1' has failed");
-		}	
-		
+		}
 	}
+
 	@Test
-	void allDividedByHundredAndNotByFourHundred2() {		
-		
+	void allDividedByHundredAndNotByFourHundred2() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 1800;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(isLeapYear) {
+		if (isLeapYear) {
 			fail("Test Case 'allDividedByHundredAndNotByFourHundred2' has failed");
-		}	
-		
+		}
 	}
+
 	@Test
-	void allDividedByHundredAndNotByFourHundred3() {		
-		
+	void allDividedByHundredAndNotByFourHundred3() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 1900;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(isLeapYear) {
+		if (isLeapYear) {
 			fail("Test Case 'allDividedByHundredAndNotByFourHundred3' has failed");
-		}	
-		
+		}
 	}
+
 	@Test
-	void allDividedByHundredAndNotByFourHundred4() {		
-		
+	void allDividedByHundredAndNotByFourHundred4() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 2100;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(isLeapYear) {
+		if (isLeapYear) {
 			fail("Test Case 'allDividedByHundredAndNotByFourHundred4' has failed");
-		}	
-		
+		}
 	}
+
 	@Test
-	void allDividedByHundredAndNotByFourHundred5() {		
-		
+	void allDividedByHundredAndNotByFourHundred5() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 2000;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(!isLeapYear) {
+		if (!isLeapYear) {
 			fail("Test Case 'allDividedByHundredAndNotByFourHundred5' has failed");
-		}	
-		
+		}
 	}
-	
+
 	@Test
-	void allDividedByFourAndNotByHundred1() {		
-		
+	void allDividedByFourAndNotByHundred1() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 2008;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(!isLeapYear) {
+		if (!isLeapYear) {
 			fail("Test Case 'allDividedByFourAndNotByHundred1' has failed");
-		}	
-		
+		}
 	}
-	
+
 	@Test
-	void allDividedByFourAndNotByHundred2() {		
-		
+	void allDividedByFourAndNotByHundred2() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 2012;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(!isLeapYear) {
+		if (!isLeapYear) {
 			fail("Test Case 'allDividedByFourAndNotByHundred2' has failed");
-		}	
-		
+		}
 	}
-	
+
 	@Test
-	void allDividedByFourAndNotByHundred3() {		
-		
+	void allDividedByFourAndNotByHundred3() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 2010;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(isLeapYear) {
+		if (isLeapYear) {
 			fail("Test Case 'allDividedByFourAndNotByHundred3' has failed");
-		}	
-		
+		}
 	}
+
 	@Test
-	void allDividedByFourAndNotByHundred4() {		
-		
+	void allDividedByFourAndNotByHundred4() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 2016;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(!isLeapYear) {
+		if (!isLeapYear) {
 			fail("Test Case 'allDividedByFourAndNotByHundred4' has failed");
-		}	
-		
+		}
 	}
-	
+
 	@Test
-	void allNotDividedByFour1() {		
-		
+	void allNotDividedByFour1() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 2017;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(isLeapYear) {
+		if (isLeapYear) {
 			fail("Test Case 'allDividedByFourAndNotByHundred4' has failed");
-		}	
-		
+		}
 	}
-	
+
 	@Test
-	void allNotDividedByFour2() {		
-		
+	void allNotDividedByFour2() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 2018;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(isLeapYear) {
+		if (isLeapYear) {
 			fail("Test Case 'allDividedByFourAndNotByHundred4' has failed");
-		}	
-		
+		}
 	}
-	
+
 	@Test
-	void allNotDividedByFour3() {		
-		
+	void allNotDividedByFour3() {
 		LeapYearAssessment leapYearAssessment = new LeapYearAssessment();
-		
 		int year = 2019;
 		boolean isLeapYear = leapYearAssessment.isLeapYear(year);
-		if(isLeapYear) {
+		if (isLeapYear) {
 			fail("Test Case 'allDividedByFourAndNotByHundred4' has failed");
-		}			
+		}
 	}
-	
 }
