9556a7ab0e145d3daa8f6be7b5c11c33f667a81f
mrmayurjain <mayurjain.2809@gmail.com>
2020-10-08 22:54:56 +0530

    Logic for first test case 'allDividedByFourHundred' is developed

--- LeapYearAssessment/src/com/mayur/incubyte/assessment/bll/LeapYearAssessment.java
+++ LeapYearAssessment/src/com/mayur/incubyte/assessment/bll/LeapYearAssessment.java
@@ -1,5 +1,14 @@
 package com.mayur.incubyte.assessment.bll;
 
 public class LeapYearAssessment {
+	
+	private static final int FOUR_HUNDRED = 400; 
+
+	public boolean isLeapYear(int year) {
+		if(year % FOUR_HUNDRED == 0) {
+			return true;
+		}
+		return false;
+	}
 
 }
